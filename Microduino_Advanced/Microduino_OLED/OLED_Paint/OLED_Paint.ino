1f30670d94e3466ab1da81250edd3a8f9dbffe70
Microduino <makermodule@gmail.com>
2014-04-03 14:55:15 +0800

    translater comment

--- Microduino_Advanced/Microduino_OLED/OLED_Paint/OLED_Paint.ino
+++ Microduino_Advanced/Microduino_OLED/OLED_Paint/OLED_Paint.ino
@@ -1,7 +1,7 @@
 #include "U8glib.h"
 #include "math.h"
 
-U8GLIB_SSD1306_128X64 u8g(U8G_I2C_OPT_NONE);//定义OLED连接方式
+U8GLIB_SSD1306_128X64 u8g(U8G_I2C_OPT_NONE);//The definition OLED connection
 
 int x,y,time,i;
 
@@ -19,20 +19,20 @@ void draw(void)
   u8g.drawLine(0,40,60,40);
   u8g.drawLine(0,0,128,0);
   u8g.drawLine(0,63,128,63);
-  //================ 图形===================//
+  //================ Graph===================//
   u8g.drawDisc(8+i,10,8);//(圆实(x,y,Diameter))
   u8g.drawCircle(26+i,10,8);//(圆虚(x,y,Diameter))
   u8g.drawFrame(18+i,46,16,16);//(长方形虚（x,y,long,wide）)
   u8g.drawBox(i,46,16,16);//(长方形实（x,y,long,wide）)
 
-  //================指针表====================//
+  //================Pointer table====================//
   u8g.drawRFrame(68,2,60,60,18);//(长方形虚（x,y,long,wide,顶角弯曲程度）)  
   u8g.drawLine(98, 2, 98, 6);//12点
   u8g.drawLine(124, 30, 128,30);//3点
   u8g.drawLine(98, 57, 98,61);//6点
   u8g.drawLine(68, 30, 72,30);//9点 
   u8g.drawLine(98, 30, 98,30);
-//================秒针====================//
+//================Seconds====================//
   x=99+16*sin(3.14*6*time/180);
   y=30-16*cos(3.14*6*time/180);
   u8g.drawLine(99,30, x, y);
