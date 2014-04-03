b80759e67fa75f2ac02b9a7bd8576767183edd9d
Microduino <makermodule@gmail.com>
2014-04-03 11:15:45 +0800

    translater comment

--- Microduino_Advanced/Microduino4Responder/Microduino4Responder.ino
+++ Microduino_Advanced/Microduino4Responder/Microduino4Responder.ino
@@ -1,28 +1,27 @@
-
 #include "SevSeg.h"
 SevSeg sevseg;
 
 void setup() {
-  //定义需要使用的引脚
+  //define pin
   sevseg.Begin(1,2,3,4,5,6,7,8,9,10,11,12,13);
-  pinMode(1, INPUT);    //按钮1引脚
-  pinMode(2, INPUT);    //按钮2引脚
-  pinMode(3, INPUT);    //按钮3引脚
-  pinMode(4, INPUT);    //按钮4引脚
+  pinMode(1, INPUT);    //button1---pin1
+  pinMode(2, INPUT);    //button2---pin2
+  pinMode(3, INPUT);    //button2---pin2
+  pinMode(4, INPUT);    //button2---pin2
 
   sevseg.PrintOutput();
-  sevseg.NewNum(0, 4); //初始值0
+  sevseg.NewNum(0, 4); //inital 0
 
 }
 
 void loop() {
-  //激活输出功能
+  //enable printout function
   sevseg.PrintOutput();
 
-  //循环读取这四个按钮的引脚
+  //Loop reads these four buttons pin
   for(int i=1; i<=4; i++) {
-    if(digitalRead(i)==HIGH) {//如果有按钮按下
-      while(1) {//显示按键编号
+    if(digitalRead(i)==HIGH) {//if push button
+      while(1) {//show button number
         sevseg.PrintOutput();
         sevseg.NewNum(i, 4);
       }
