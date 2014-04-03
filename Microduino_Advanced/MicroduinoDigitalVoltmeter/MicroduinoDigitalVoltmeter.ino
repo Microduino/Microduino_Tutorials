48c6d65754835339e1cd90ce85be01be0bb5dab3
Microduino <makermodule@gmail.com>
2014-04-03 13:12:15 +0800

    translater comment

--- Microduino_Advanced/MicroduinoDigitalVoltmeter/MicroduinoDigitalVoltmeter.ino
+++ Microduino_Advanced/MicroduinoDigitalVoltmeter/MicroduinoDigitalVoltmeter.ino
@@ -1,29 +1,30 @@
     /*
-     作用:介绍使用arduino模拟口测量电压，作为0-5V电压表使用实验
+     useage: To introduce the use of microduino analog port to measure voltage, 0-5V voltage as a table using the experimental
     */
      
-    float temp;   //创建一个浮点型变量temp作为存储空间准备存放数据
+    float temp;   //Create a float variable temp as a storage space to store data preparation
     void setup()
     {
-      Serial.begin(9600);     //使用9600的波特率进行串口通讯
+      Serial.begin(9600);
     }
     void loop()
     {
      
       int V1 = analogRead(A0);                    
-    //从A0口读取电压数据存入刚刚创建整数型变量V1，模拟口的电压测量范围为0-5V 返回的值为0-1024
+    //
+A0 reads the voltage from the mouth into the newly created data type integer variables V1, analog voltage measurement range of ports 0-5V return value 0-1024
       float vol = V1*(5.0 / 1023.0);               
-    //我们将 V1的值换算成实际电压值存入浮点型变量 vol
+    //We will be converted into the actual voltage value V1 value into a float variable vol
       if (vol == temp)                             
-    //这部分的判断是用来过滤重复的数据，只有本次的电压值和上次不一时才进行输出
+    //This part of the judgment is used to filter duplicate data, only the second and last time varying voltage value when output
       {
-        temp = vol;                               //比较完成后，将这次的值存入比对比用的变量temp
+        temp = vol;
       }
       else
       {
-        Serial.print(vol);                       //串口输出电压值，并且不换行  
-        Serial.println(" V");                    //串口输出字符V，并且换行
+        Serial.print(vol);                       //Serial output voltage value, and does not wrap
+        Serial.println(" V");                    //Serial output character V, and wrap
         temp = vol;
-        delay(1000);                           //输出完成后等待1秒钟，用于控制数据的刷新速度。
+        delay(1000);                           //Wait a second output after the completion, the refresh rate for the control data.
       }
     }
