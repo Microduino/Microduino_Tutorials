9f243fd9a099cb0faaba8537d25f6e8c5409897d
Microduino <makermodule@gmail.com>
2014-04-03 14:16:33 +0800

    translater comment

--- Microduino_Advanced/MicroduinoIRReceiver/MicroduinoIRReceiver.ino
+++ Microduino_Advanced/MicroduinoIRReceiver/MicroduinoIRReceiver.ino
@@ -1,28 +1,21 @@
-//本例来自于红外接收模块的IRremote自带范例
-/*
- * IRremote: IRrecvDemo - demonstrates receiving IR codes with IRrecv
- * An IR detector/demodulator must be connected to the input RECV_PIN.
- * Version 0.1 July, 2009
- * Copyright 2009 Ken Shirriff
- * [url]http://arcfn.com[/url]
- */
+
  
 #include <IRremote.h>
  
-int RECV_PIN = 11;//定义红外接收器的引脚为11
+int RECV_PIN = 11;//Defined infrared receiver pin 11
 IRrecv irrecv(RECV_PIN);
 decode_results results;
 
 void setup()
 {
   Serial.begin(9600);
-  irrecv.enableIRIn(); // 初始化红外接收器
+  irrecv.enableIRIn(); // Initialization infrared receiver
 }
 
 void loop() {
   if (irrecv.decode(&results)) {
-    Serial.println(results.value, HEX);//以16进制换行输出接收代码
-    Serial.println();//为了便于观看输出结果增加一个空行
-    irrecv.resume(); // 接收下一个值
+    Serial.println(results.value, HEX);//Wrap output in hexadecimal code receiver
+    Serial.println();
+    irrecv.resume(); // receive next value
   }
 }
