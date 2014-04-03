58592ea4301c7a511adb111f74e3d3fd65ecf9bf
Microduino <makermodule@gmail.com>
2014-04-03 12:02:00 +0800

    translater comment

--- Microduino_Advanced/MicroduinoDCMotorRevers/MicroduinoDCMotorRevers.ino
+++ Microduino_Advanced/MicroduinoDCMotorRevers/MicroduinoDCMotorRevers.ino
@@ -1,9 +1,9 @@
-int motorPin1 =  5;    // 一条线连接 digital pin 5
-int motorPin2 =  6;    // 另一条线连接 digital pin 6
+int motorPin1 =  5;    // one wire connect to digital pin 5
+int motorPin2 =  6;    // another connect to digital pin 6
 
 
 void setup()   {                
-  // 初始化，设置两个引脚为输出
+  // inital, set to pins is OUTPUT
   pinMode(motorPin1, OUTPUT); 
   pinMode(motorPin2, OUTPUT);  
 }
@@ -11,41 +11,41 @@ void setup()   {
 
 void loop()                     
 {
-  rotateLeft(150, 500);//左转速率150,持续500ms
-  rotateRight(50, 1000);//右转速率50，持续1000ms
-  rotateRight(150, 1000);//右转速率150,持续1000ms
-  rotateRight(200, 1000);//右转速率200,持续1000ms
-  rotateLeft(255, 500);//左转速率255,持续500ms
-  rotateRight(10, 1500);//右转速率10，持续1500ms
+  rotateLeft(150, 500);//Turn left rates 150,delay 500ms
+  rotateRight(50, 1000);//Turn right rates 50，delay 1000ms
+  rotateRight(150, 1000);//Turn right rates 150, delay 1000ms
+  rotateRight(200, 1000);//Turn right rates 200, delay 1000ms
+  rotateLeft(255, 500);//Turn left rates 255,delay 500ms
+  rotateRight(10, 1500);//Turn right rates 10，delay 1500ms
 }
 
-//向左转
+//rotate left
 void rotateLeft(int speedOfRotate, int length){
-  analogWrite(motorPin1, speedOfRotate); //用pwm设置电流大小
-  digitalWrite(motorPin2, LOW);    // 设置 Pin motorPin2为LOW
+  analogWrite(motorPin1, speedOfRotate); //use pwm set current
+  digitalWrite(motorPin2, LOW);    // set Pin motorPin2 is LOW
   delay(length); //waits
-  digitalWrite(motorPin1, LOW);    // 设置 Pin motorPin1为LOW
+  digitalWrite(motorPin1, LOW);    // set Pin motorPin1 is LOW
 }
-//向右转
+//rotate right
 void rotateRight(int speedOfRotate, int length){
-  analogWrite(motorPin2, speedOfRotate); //用pwm设置电流大小
-  digitalWrite(motorPin1, LOW);    // 设置 Pin motorPin1为LOW
+  analogWrite(motorPin2, speedOfRotate); //use pwm set current
+  digitalWrite(motorPin1, LOW);    // set Pin motorPin1 is LOW
   delay(length); //waits
-  digitalWrite(motorPin2, LOW);    // 设置 Pin motorPin2为LOW
+  digitalWrite(motorPin2, LOW);    // set Pin motorPin2 is LOW
 }
 
-//全力向左转
+//full rotate left
 void rotateLeftFull(int length){
-  digitalWrite(motorPin1, HIGH); //设置最大电流
-  digitalWrite(motorPin2, LOW);    // 设置 Pin motorPin2为LOW
+  digitalWrite(motorPin1, HIGH); //set max current
+  digitalWrite(motorPin2, LOW);    // set Pin motorPin2 is LOW
   delay(length); //waits
-  digitalWrite(motorPin1, LOW);    // 设置 Pin motorPin1为LOW
+  digitalWrite(motorPin1, LOW);    // set Pin motorPin1 is LOW
 }
-//全力向右转
+//full rotate right
 void rotateRightFull(int length){
-  digitalWrite(motorPin2, HIGH); //设置最大电流
-  digitalWrite(motorPin1, LOW);    // 设置 Pin motorPin1为LOW
+  digitalWrite(motorPin2, HIGH); //set max current
+  digitalWrite(motorPin1, LOW);    // set Pin motorPin1 is LOW
   delay(length); //waits
-  digitalWrite(motorPin2, LOW);    // 设置 Pin motorPin2为LOW
+  digitalWrite(motorPin2, LOW);    // set Pin motorPin2 is LOW
 }
 
