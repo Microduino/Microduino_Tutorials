604ff0978cd0615eea45868bd97775c1e4dfa77f
Microduino <makermodule@gmail.com>
2014-04-03 14:38:56 +0800

    translater comment

--- Microduino_Advanced/MicroduinoSpeedMeasurement/MicroduinoSpeedMeasurement.ino
+++ Microduino_Advanced/MicroduinoSpeedMeasurement/MicroduinoSpeedMeasurement.ino
@@ -1,19 +1,19 @@
 #include "U8glib.h"
 
 
-int potPin = A0;//电位器引脚定义
-int motorPin = 9;//电机引脚定义
-int potValue = 0;//电位器的数值
-int motorValue = 0;//电机的数值
+int potPin = A0;//Potentiometer pin definitions
+int motorPin = 9;//Motor pin definitions
+int potValue = 0;//The value of the potentiometer
+int motorValue = 0;//Value of the motor
 
-U8GLIB_SSD1306_128X64 u8g(U8G_I2C_OPT_NONE);//定义OLED连接方式
+U8GLIB_SSD1306_128X64 u8g(U8G_I2C_OPT_NONE);//The definition OLED connection
 
-//显示速度
+//show speed
 void draw(int speedNum) {
 
-  u8g.setFont(u8g_font_unifont);//字体1
+  u8g.setFont(u8g_font_unifont);//font1
   u8g.drawStr( 0, 16, "Speed:");
-  u8g.setFont(u8g_font_7x13);//字体2
+  u8g.setFont(u8g_font_7x13);//font2
   u8g.setPrintPos(0, 32);
   u8g.print(speedNum);
   u8g.print(" (rpm)");
@@ -27,9 +27,9 @@ void setup()
 
 void loop()
 {
-  //读取电位器的值
+  //Reading the value of the potentiometer
   potValue = analogRead(potPin);  
-  //映射为电机的速率值
+  //Mapped to the value of the motor speed
   motorValue = map(potValue, 0, 1023, 0, 200);
   analogWrite(motorPin, motorValue);
 
