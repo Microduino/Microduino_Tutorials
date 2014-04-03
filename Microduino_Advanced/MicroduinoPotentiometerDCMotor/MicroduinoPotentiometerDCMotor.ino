de39785accb17910b3a8a9e302e3d0679679cd55
Microduino <makermodule@gmail.com>
2014-04-03 14:27:28 +0800

    translater comment

--- Microduino_Advanced/MicroduinoPotentiometerDCMotor/MicroduinoPotentiometerDCMotor.ino
+++ Microduino_Advanced/MicroduinoPotentiometerDCMotor/MicroduinoPotentiometerDCMotor.ino
@@ -1,18 +1,18 @@
-int potPin = A0; //电位器输入引脚
-int motorPin = 9;//电机引脚
-int potValue = 0;//定义电位器的值
-int motorValue = 0;//记录电机的值
+int potPin = A0; //Potentiometer input pin
+int motorPin = 9;//Motor pin
+int potValue = 0;//Defined potentiometer value
+int motorValue = 0;//Value recorded motor
 void setup() {
  Serial.begin(9600);
 }
 void loop() {
- potValue = analogRead(potPin);  //读取电位器的值
- motorValue = map(potValue, 0, 1023, 0, 255);//映射为0~255之间的数值
- analogWrite(motorPin, motorValue);//PWM传给电机
- //显示
- Serial.print("potentiometer = " );     
+ potValue = analogRead(potPin); //Reading the value of the potentiometer
+ motorValue = map(potValue, 0, 1023, 0, 255);//Mapped to a value between 0 and 255
+ analogWrite(motorPin, motorValue);//PWM pass motor 
+ //display
+ Serial.print("potentiometer = " );
  Serial.print(potValue);
  Serial.print("\t motor = ");
  Serial.println(motorValue);
- delay(2);    
+ delay(2);
 }
