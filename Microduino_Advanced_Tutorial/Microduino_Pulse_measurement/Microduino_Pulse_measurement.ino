74c3fc2ef115fe2fe7f02cf53ec48dc3567a646a
Microduino <support@microduino.cc>
2014-05-04 22:25:56 +0800

    microduino
    microduino

--- Microduino_Advanced_Tutorial/Microduino_Pulse_measurement/Microduino_Pulse_measurement.ino
+++ Microduino_Advanced_Tutorial/Microduino_Pulse_measurement/Microduino_Pulse_measurement.ino
@@ -1,43 +1,15 @@
+#include "U8glib.h"
 
-/*
->> Pulse Sensor Amped 1.2 <<
- This code is for Pulse Sensor Amped by Joel Murphy and Yury Gitman
- www.pulsesensor.com 
- >>> Pulse Sensor purple wire goes to Analog Pin 0 <<<
- Pulse Sensor sample aquisition and processing happens in the background via Timer 2 interrupt. 2mS sample rate.
- PWM on pins 3 and 11 will not work when using this code, because we are using Timer 2!
- The following variables are automatically updated:
- Signal :    int that holds the analog signal data straight from the sensor. updated every 2mS.
- IBI  :      int that holds the time interval between beats. 2mS resolution.
- BPM  :      int that holds the heart rate value, derived every beat, from averaging previous 10 IBI values.
- QS  :       boolean that is made true whenever Pulse is found and BPM is updated. User must reset.
- Pulse :     boolean that is true when a heartbeat is sensed then false in time with pin13 LED going out.
- 
- This code is designed with output serial data to Processing sketch "PulseSensorAmped_Processing-xx"
- The Processing sketch is a simple data visualizer. 
- All the work to find the heartbeat and determine the heartrate happens in the code below.
- Pin 13 LED will blink with heartbeat.
- If you want to use pin 13 for something else, adjust the interrupt handler
- It will also fade an LED on pin fadePin with every beat. Put an LED and series resistor from fadePin to GND.
- Check here for detailed code walkthrough:
- http://pulsesensor.myshopify.com/pages/pulse-sensor-amped-arduino-v1dot1
- 
- Code Version 1.2 by Joel Murphy & Yury Gitman  Spring 2013
- This update fixes the firstBeat and secondBeat flag usage so that realistic BPM is reported.
- 
- */
+U8GLIB_SSD1306_128X64 u8g(U8G_I2C_OPT_NONE);	
 
-
-//  VARIABLES
 int pulsePin = A0;                 // Pulse Sensor purple wire connected to analog pin 0
-int blinkPin = 6;                // pin to blink led at each beat
-int fadePin = 5;                  // pin to do fancy classy fading blink at each beat
+int blinkPin = 5;                // pin to blink led at each beat
+int fadePin = 6;                  // pin to do fancy classy fading blink at each beat
 int fadeRate = 0;                 // used to fade LED on with PWM on fadePin
 
 int data,num;
 
-float timer,timey,time1,time2,time3;  //定义变量为浮点数
-
+long timer,timey,time1,time2,time3;  //定义变量为浮点数
 
 // these variables are volatile because they are used during the interrupt service routine!
 volatile int BPM;                   // used to hold the pulse rate
@@ -46,11 +18,27 @@ volatile int IBI = 600;             // holds the time between beats, must be see
 volatile boolean Pulse = false;     // true when pulse wave is high, false when it's low
 volatile boolean QS = false;        // becomes true when Arduoino finds a beat.
 
+//阴码、逐行、逆向
+static unsigned char u8g_logo_bits1[] U8G_PROGMEM = {
+  0x00,0x00,0x00,0x00,0x00,0x00,0x78,0x3C,
+  0xFC,0x7E,0xFE,0xFF,0xFE,0xFF,0xFE,0xFF,
+  0xFE,0xFF,0xFC,0x7F,0xF8,0x3F,0xF0,0x1F,
+  0xE0,0x0F,0xC0,0x07,0x80,0x03,0x00,0x01//未命名文件",0//
+};
+static unsigned char u8g_logo_bits2[] U8G_PROGMEM = {
+  0xFC,0x3F,0x02,0x41,0x05,0xA1,0x01,0x80,
+  0x01,0x80,0x81,0x80,0x81,0x80,0x87,0xE0,
+  0x81,0x80,0x01,0x81,0x01,0x82,0x01,0x84,
+  0x01,0x88,0x05,0xA1,0x02,0x41,0xFC,0x3F,//未命名文件0//
+};
 
 void setup(){
+  //u8g.setRot180();
   timer=millis();
   timey=millis();
   time1=millis();
+  time2=millis();
+  time3=millis();
   pinMode(blinkPin,OUTPUT);         // pin that will blink to your heartbeat!
   pinMode(fadePin,OUTPUT);          // pin that will fade to your heartbeat!
   pinMode(pulsePin,  INPUT_PULLUP);
@@ -61,15 +49,38 @@ void setup(){
   //analogReference(EXTERNAL);   
 }
 
+void draw(void) { 
+  
+  u8g.drawXBMP( 90, 0, 16, 16, u8g_logo_bits1);
+  u8g.drawXBMP( 90, 20, 16, 16, u8g_logo_bits2);
 
+  u8g.setFont(u8g_font_fub20);
+  u8g.setPrintPos(32, 64); 
+  u8g.print(num); 
+  u8g.setFont(u8g_font_9x18B);
+  u8g.print("N/min");
+  u8g.setFont(u8g_font_7x14);
+  u8g.setPrintPos(0, 16); 
+  u8g.print("Heat:");
+  u8g.setFont(u8g_font_9x18B);
+  u8g.print(data);
+  u8g.setFont(u8g_font_7x14);
+  u8g.print("N");
+  u8g.setPrintPos(0, 34);  
+  u8g.print("Time:");
+  u8g.setFont(u8g_font_9x18B);
+  u8g.print(timey);
+  u8g.setFont(u8g_font_7x14);
+  u8g.print("S");
+}
 
 void loop(){
-  sendDataToProcessing('S', Signal);     // send Processing the raw Pulse Sensor data
+
   if (QS == true){                       // Quantified Self flag is true when arduino finds a heartbeat
 
     fadeRate = 255;                  // Set 'fadeRate' Variable to 255 to fade LED with pulse
-    sendDataToProcessing('B',BPM);   // send heart rate with a 'B' prefix
-    sendDataToProcessing('Q',IBI);   // send time between beats with a 'Q' prefix
+
+
     QS = false;                      // reset the Quantified Self flag for next time    
   }
 
@@ -77,36 +88,36 @@ void loop(){
 
   if(fadeRate>230)   
     data++;
-  if(data>5)
+  if(data>0)
   {  
-    
+
     if(millis()-timer>1000)
     {
       timer=millis();
       timey++;
     }
-    if(data%5==0)
-      num=60*(data-5)/timey;
+    if(timey>10)
+      num=60*(data)/timey;
   }
- if(fadeRate==0)
- {
-     if(millis()-time2>1000)
+  if(fadeRate==0)
+  {
+    if(millis()-time2>1000)
     {
       time2=millis();
       time3++;
     }
- }
- else
- time3=0;
- 
-  if(time3>1)
+  }
+  else
+    time3=0;
+
+  if(time3>1||timey>60)
   {
     data=0;
     num=0;
     timey=0;
     time3=0;
   }
- 
+
 
   delay(20);                             //  take a break
   Serial.print("xintiao:"); 
@@ -115,11 +126,16 @@ void loop(){
   Serial.println(fadeRate);  
   Serial.print("   timey: ");  
   Serial.print(timey);
-   Serial.print("   time3: ");  
+  Serial.print("   time3: ");  
   Serial.println(time3);  
   Serial.print("   xintiao: ");  
   Serial.print(num); 
   Serial.println("num/min");  
+  u8g.firstPage();  
+  do {
+    draw();
+  } 
+  while( u8g.nextPage() );
 }
 
 
@@ -127,13 +143,17 @@ void loop(){
 void ledFadeToBeat(){
   fadeRate -= 15;                         //  set LED fade value
   fadeRate = constrain(fadeRate,0,255);   //  keep LED fade value from going into negative numbers!
-  analogWrite(fadePin,fadeRate);          //  fade LED
+  //analogWrite(fadePin,fadeRate);          //  fade LED
+  if(fadeRate>200)
+    digitalWrite(fadePin,HIGH);
+  else
+    digitalWrite(fadePin,LOW);
 }
 
-void sendDataToProcessing(char symbol, int data ){
-  //Serial.print(symbol);                // symbol prefix tells Processing what type of data is coming
-  // Serial.println(data);                // the data to send culminating in a carriage return
-}
+
+
+
+
 
 
 
